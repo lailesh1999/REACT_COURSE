45291d3faf86fe56e39afa7c68675ab2f633b84f
lailesh1999 <laileshveigas1999@gmail.com>
2023-06-15 18:14:09 +0530

    15

--- App.js
+++ App.js
@@ -1,29 +1,37 @@
-import React from "react";
+import {createElement} from "react";
 import  ReactDOM  from "react-dom/client";
 
 
-const heading = React.createElement(
+const heading = createElement(
     "h1",{
             id:"heading1",
             key:"h1"
         }," heading 1  "
         );
 
-const heading1 = React.createElement(
+const heading1 = createElement(
     "h1",
 {
     id : "heading2",
     key:"h2"
 },"heading 2" );
 
-const  container1 = React.createElement(
+
+const heading2 = (
+<h1 id="title" key="h2">HELLO REACT dd</h1>
+);
+
+
+const  container1 = createElement(
     "div",
 {
     id:"container",
    // style:{color:"red",backgroundColor:"grey",}
 },
-[heading]
+
+[heading,heading1,heading2]
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //passing react element inside the  root 
 root.render(container1);
