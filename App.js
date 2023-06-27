d45f4e5db706fad42ea28c79705c6a0e9a348efe
lailesh1999 <laileshveigas1999@gmail.com>
2023-06-27 12:38:28 +0530

    index key

--- App.js
+++ App.js
@@ -155,15 +155,14 @@ const Body = () =>{
         <div className="restaurent-list">
             {
                 // res is object of restaurentList for exmpale resturentList[0] as one object and resturentList[1] is second object
-                restaurentList.map(res=>{
-                    return <RestrauntCard  {...res} />
-                })
-            }
+                restaurentList.map((res,index) => {
+                    return <RestrauntCard  {...res} key={index} />;
+                })}
                 
         </div>
             
     );
-}
+};
 
    const Footer = () =>{
     return (
