a25c6a4fb8d126073045611dcee08dcfcfe53013
lailesh1999 <laileshveigas1999@gmail.com>
2023-06-28 18:10:47 +0530

    hooks state varibale sync

--- src/App.js
+++ src/App.js
@@ -27,79 +27,9 @@ import Body from "./components/Body";
                  
 
                 */
-// const heading = React.createElement(
-//     "h1",{
-//             id:"heading1",
-//             key:"h1"
-//         }," heading 1  "
-//         );
-
-// const heading1 = React.createElement(
-//     "h1",
-// {
-//     id : "heading2",
-//     key:"h2"
-// },"heading 2" );
-                        
-//jsx expression
-//reactElement
 
 
 
-
-
-//config Driven UI 
-
-// const config =[
-//     {
-//         type:"carousel",
-//         card:[
-//             {
-//                 offerName:"50% off"
-//             },
-//             {
-//                 offerName:"No Delivery Charge"
-//             }
-//         ]
-//     },
-//     {
-//         type:"restaurent",
-//         card:[
-//             {
-//                 name:"BURGER KING",
-//                 image:"https://cdn.tasteatlas.com/images/toplistarticles/08c818739e4b48ce96d319c16f4cc0ca.jpg?m=facebook",
-//                 cusiness:["Burger","American"],
-//                 rating:"4.2"
-//             },
-//             {
-//                 name:"KFC",
-//                 image:"https://cdn.tasteatlas.com/images/toplistarticles/08c818739e4b48ce96d319c16f4cc0ca.jpg?m=facebook",
-//                 cusiness:["Burger","American"],
-//                 rating:"4.2"
-//             },
-//         ]
-//     }
-// ]
-
-
-
-
-//props
-// const Body = () =>{
-//     return (
-//         <div className="restaurent-list">
-//             {
-//                 // res is object of restaurentList for exmpale resturentList[0] as one object and resturentList[1] is second object
-//                 restaurentList.map((res,index) => {
-//                     return <RestrauntCard  {...res} key={index} />;
-//                 })}
-                
-//         </div>
-            
-//     );
-// };
-
-   
 
 const AppLayout = () =>{
     return (
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,19 @@
 import { restaurentList } from "../components/constant";
 import { RestrauntCard } from "./RestrauntCard";
+import { useState} from "react";
 //props
 const Body = () =>{
+    //const searchTxt = "KFC";
+    //searchText is a local state varibale
+    const [searchInput,setSearchInput] = useState("KFC");//return array-array has first element 
+    //has vriable name and the second element is setFunction to update the varibale and we call it as state varibale
+
     return (
+    <>
+        <div className="search-container">
+            <input type="text" className="search-input"  name="dd" placeholder="search" onChange={(e)=>{setSearchInput(e.target.value)}} value={searchInput} />
+            <button className="search-btn">SEARCH</button>
+        </div>
         <div className="restaurent-list">
             {
                 // res is object of restaurentList for exmpale resturentList[0] as one object and resturentList[1] is second object
@@ -11,7 +22,7 @@ const Body = () =>{
                 })}
                 
         </div>
-            
+    </>  
     );
 };
 
